fa4baed91d6d143cc63fc1b130820cd500faee6f
Didier Gaspard <d.gaspard@proximaweb.mu>
2018-11-09 14:40:01 +0400

    Added dev/prod config
    Added dev/prod config based on environment

--- web/app.php
+++ web/app.php
@@ -6,10 +6,10 @@ require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
-Debug::enable();
+//Debug::enable();
 
-$kernel = new AppKernel('dev', true);
-//$kernel = new AppKernel('prod', false);
+//$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
